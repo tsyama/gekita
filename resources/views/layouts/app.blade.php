8499593e7572fcdef229ee91e1087940658660be
tsyama <tsyama.desu@gmail.com>
2019-02-21 01:18:09 +0900

    home/indexの作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<html lang="{{ app()->getLocale() }}">
+<head>
+    @if($__env->yieldContent('title') === '')
+        <title>gekita</title>
+    @else
+        <title>@yield('title') - gekita</title>
+    @endif
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                GEKITA
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                </form>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="{{ url('posts') }}">{{ __('Posts') }}</a>
+                    <a class="nav-link" href="{{ url('users') }}">{{ __('Users') }}</a>
+                </div>
+            </div>
+        </nav>
+        <main class="py-4">
+            <div class="container">
+                @yield('content')
+            </div>
+        </main>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
